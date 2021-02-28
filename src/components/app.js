7f789dd9b26377b94e810a1db4f1a49b996abc4b
Corentin Girard <corentingirard.dev@gmail.com>
2021-02-28 22:20:51 +0100

    Start with Material Design

--- src/components/app.js
+++ src/components/app.js
@@ -2,20 +2,15 @@ import { h } from 'preact';
 import { Router } from 'preact-router';
 
 import Header from './header';
-
-// Code-splitting is automated for `routes` directory
 import Home from '../routes/home';
-import Profile from '../routes/profile';
 
 const App = () => (
-	<div id="app">
-		<Header />
-		<Router>
-			<Home path="/" />
-			<Profile path="/profile/" user="me" />
-			<Profile path="/profile/:user" />
-		</Router>
-	</div>
+    <div id="app">
+        <Header />
+        <Router>
+            <Home path="/" />
+        </Router>
+    </div>
 )
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import './style';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
+import PrimeReact from 'primereact/api';
+PrimeReact.ripple = true;
+
 import App from './components/app';
 
 export default App;
