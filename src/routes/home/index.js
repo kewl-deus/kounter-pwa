291011471daa51e504b5dc30c577253517376800
Corentin Girard <corentingirard.dev@gmail.com>
2021-03-01 00:27:43 +0100

    Add dialog

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -3,6 +3,10 @@ import { useState } from 'preact/hooks';
 
 import style from './style.css';
 
+import { Dialog } from 'primereact/dialog';
+import { Toolbar } from 'primereact/toolbar';
+import { FileUpload } from 'primereact/fileupload';
+import { Button } from 'primereact/button';
 import { Dropdown } from 'primereact/dropdown';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
@@ -66,14 +70,48 @@ const Home = () => {
     );
   }
 
+  const [fileDialog, setFileDialog] = useState(false);
+
+  const importFile = () => {
+    // Import file
+
+    setFileDialog(false);
+  }
+
+  const openDialog = () => {
+    setFileDialog(true);
+  }
+
+  const hideDialog = () => {
+    setFileDialog(false);
+  }
+
+  const fileDialogFooter = (
+    <>
+      <Button label="Cancel" icon="pi pi-times" className="p-button-text" onClick={hideDialog} />
+      <Button label="Save" icon="pi pi-check" className="p-button-text" onClick={importFile} />
+    </>
+  );
+
+  const leftToolbarTemplate = <Dropdown placeholder="Select a list" value={selected} options={lists} onChange={e => setSelected(e.value)}></Dropdown>
+  const rightToolbarTemplate = <Button label="Import file" icon="pi pi-plus" className="p-button-success p-mr-2" onClick={openDialog} />
+
   return (
     <main class={style.home}>
-      <Dropdown placeholder="Select a list" value={selected} options={lists} onChange={e => setSelected(e.value)}></Dropdown>
+      <Toolbar className="p-mb-4" left={leftToolbarTemplate} right={rightToolbarTemplate}></Toolbar>
+
 
       <DataTable value={data[selected]}>
         <Column field="name" header="Name"></Column>
         <Column field="counter" header="Counter" body={counterBodyTemplate}></Column>
       </DataTable>
+
+      <Dialog visible={fileDialog} style={{ width: '450px' }} header="Import file" modal class="p-fluid" footer={fileDialogFooter} onHide={hideDialog}>
+        <div className="p-field">
+          <label htmlFor="name">Name</label>
+          <FileUpload mode="basic" accept=".xls,.xlsx,.csv" label="Import" chooseLabel="Import" className="p-mr-2 p-d-inline-block" />
+        </div>
+      </Dialog>
     </main>
   )
 };
